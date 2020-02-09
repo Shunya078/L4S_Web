c95e2c6f320a968f562a32d8994cb7199fd8bb47
kojitaniguchi <tanigu0511@gmail.com>
2020-02-09 23:05:28 +0900

    Add webrick-app

---
+++ Day1/practice/webrick-app/app.rb
@@ -0,0 +1,9 @@
+require 'webrick'
+
+srv = WEBrick::HTTPServer.new({ :DocumentRoot => './',
+                                :BindAddress => '127.0.0.1',
+                                :Port => 20080})
+srv.mount('/index.html', WEBrick::HTTPServlet::FileHandler, 'index.html')
+
+trap("INT"){ srv.shutdown }
+srv.start
